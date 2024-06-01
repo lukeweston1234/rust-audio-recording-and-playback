e273229d8f214e5af2cc52d34dd5faa88508265a
Luke Weston <lukeweston1234@gmail.com>
2024-06-01 15:32:02 -0400

    audio recording

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod audio_clip;
+
+use audio_clip::AudioClip;
+use color_eyre::eyre::Result;
+
+fn main() -> Result<()>{
+    color_eyre::install(); // Error handling and logging
+
+    let clip = AudioClip::record()?;
+
+    clip.play();
+
+    Ok(())
 }
